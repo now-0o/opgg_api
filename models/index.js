b1f4b651d2b31453e69edde6c103eb4f67121380
now-0o <dlwpdn0704@gmail.com>
2023-12-12 21:11:23 +0900

    refactor: ban 테이블의 컬럼을 champGameData 테이블에 병합

--- models/index.js
+++ models/index.js
@@ -4,7 +4,6 @@ const Rank = require('./Rank');
 const Tier = require('./Tier');
 const Lane = require('./Lane');
 const Game = require('./Game');
-const Ban = require('./Ban');
 const ChampGameData = require('./ChampGameData');
 const Rotation = require('./Rotation');
 
@@ -26,8 +25,13 @@ ChampGameData.belongsTo(Lane);
 User.hasMany(ChampGameData);
 ChampGameData.belongsTo(User);
 
-Champion.hasMany(Ban);
-Ban.belongsTo(Champion);
+// 첫 번째 참조 관계
+Champion.hasMany(ChampGameData, { as: 'pickChampion', foreignKey: 'pickId' });
+ChampGameData.belongsTo(Champion, { foreignKey: 'pickId' });
+
+// 두 번째 참조 관계
+Champion.hasMany(ChampGameData, { as: 'banChampion', foreignKey: 'banId' });
+ChampGameData.belongsTo(Champion, { foreignKey: 'banId' });
 
 Champion.belongsToMany(Lane, {through : "champion_lane"});
 Lane.belongsToMany(Champion, {through : "champion_lane"});
@@ -46,6 +50,5 @@ module.exports = {
     Lane,
     Game,
     ChampGameData,
-    Ban,
     Rotation
 }
