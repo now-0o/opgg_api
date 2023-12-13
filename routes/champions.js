33575a03e3ec81e37372f7b2921fcd154ea88fa1
now-0o <dlwpdn0704@gmail.com>
2023-12-13 23:05:23 +0900

    feat: 최근 검색 챔피언 저장, 조회 라우트 추가

--- routes/champions.js
+++ routes/champions.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { Champion, Lane, Rotation } = require('../models');
+const { Champion, Lane, Rotation, User, SearchHistory } = require('../models');
 const { Op } = require('sequelize');
 const sequelize = require('../config/database');
 const HttpException = require('../HttpException');
 const asyncHandler = require('../utils/asyncHandler');
+const authenticateToken = require('../middlewares/authenticateToken');
 
 router.get('/', asyncHandler(async (req, res) => {
     const searchedChampions = await Champion.findAll({
@@ -16,7 +17,7 @@ router.get('/', asyncHandler(async (req, res) => {
 
 router.get('/search', asyncHandler(async (req, res) => {
     // 비구조화할당 
-    const {champion} = req.body;
+    const {champion} = req.query;
     
     if(!champion){
       throw new HttpException(400, '검색할 챔피언명이 없습니다.')
@@ -39,7 +40,7 @@ router.get('/search', asyncHandler(async (req, res) => {
   }));
 
   router.get('/search/lane', asyncHandler(async (req, res) => {
-     const {laneId} = req.body;
+     const { laneId } = req.body;
 
      if(!laneId){
         throw new HttpException(400, '조회할 라인의 값이 없습니다.');
@@ -53,7 +54,7 @@ router.get('/search', asyncHandler(async (req, res) => {
             where: {
                 id : laneId
             },
-            attributes: ['lane'], // 이 코드를 남겨둔 이유 ?
+            attributes: ['lane'], 
             },
             {
             model: Rotation,
@@ -65,4 +66,53 @@ router.get('/search', asyncHandler(async (req, res) => {
      res.status(200).send(searchedChampions);
   }));
 
-  module.exports = router;
+  router.post('/save-recent-search', authenticateToken, asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const {championId} = req.body;
+
+    if(!championId){
+      throw new HttpException(400, "챔피언 ID는 필수값 입니다.");
+    }
+    
+    const result = await sequelize.transaction(async () => {
+      const foundChampion = await Champion.findByPk(championId);
+
+      if(!foundChampion){
+          throw new HttpException(400, '존재하지 않는 챔피언의 ID입니다.');
+      }
+
+      const savedDatetime = new Date();
+
+      const savedRecentSearch = await SearchHistory.create({
+        userId: userId,
+        championId: championId,
+        createdAt: savedDatetime,
+      });
+
+      return savedRecentSearch;
+    });
+
+    res.status(201).json(result);
+  }));
+
+  router.get('/get-recent-search', authenticateToken, asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+
+    const recentSearch = await SearchHistory.findAll({
+        attributes: ['userId', 'championId', 'createdAt'],
+        where: {
+            userId
+        },
+        include: [
+            {
+                model: Champion,
+                attributes: ['id', 'name', 'image']
+            }
+        ],
+        limit: 3
+    });
+
+    res.status(200).json(recentSearch);
+}));
+
+module.exports = router;
